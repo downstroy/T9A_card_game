4c2d4b2436370ac605f58bb8559079dc6e86b04e
downstroy <downstroypvp@hotmail.fr>
2018-12-19 07:59:27 +0100

    unity workspace

---
+++ Assets/script/Card.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+    public int life;
+    public int damage;
+    public int rank;
+
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text = life.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
